c09da37f4b353df0d06b33305ba20e8ba574597a
AribahS <aribahshirgaonker@gmail.com>
2026-06-18 19:20:39 +0530

    Pattern Problems

---
+++ Patterns/Pattern7.java
@@ -0,0 +1,34 @@
+package Patterns;
+
+import java.util.Scanner;
+
+/*
+    *
+   ***
+  *****
+ *******
+*********
+ */
+public class Pattern7 {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the value of N: ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i -1; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2 * i + 1; k++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < n - i -1; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
