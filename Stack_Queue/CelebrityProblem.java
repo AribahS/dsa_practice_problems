7df4fa7cf2c16a4e3316293734a77689fbf6e665
AribahS <aribahshirgaonker@gmail.com>
2026-09-01 20:44:19 +0530

    Celebrity Problem

--- Stack_Queue/CelebrityProblem.java
+++ Stack_Queue/CelebrityProblem.java
@@ -1,5 +1,10 @@
 package Stack_Queue;
-import java.util.Stack;
+/*
+Problem Statement: A celebrity is a person who is known by everyone else at the party but does not
+know anyone in return. Given a square matrix M of size N x N where M[i][j] is 1 if person i knows 
+person j, and 0 otherwise, determine if there is a celebrity at the party. 
+Return the index of the celebrity or -1 if no such person exists.
+*/
 public class CelebrityProblem {
     public int celebrity(int M[][]){
         int n=M.length;
